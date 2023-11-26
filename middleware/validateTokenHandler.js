d7728f3ebb6e926f287ca05a4a500e4910e03008
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2023-11-26 12:35:34 +0530

    demo is updated

---
+++ middleware/validateTokenHandler.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+
+const validateToken = asyncHandler(async (req, res, next) => {
+  let token;
+  let authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    // Retrieve the token from the authorization bearer
+    token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401);
+        throw new Error('User is not authorized');
+      }
+      req.user = decoded.user;
+      next();
+    });
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('User is not authorized or token is missing');
+  }
+});
+
+module.exports = validateToken;
